e0923e6e83764541852c25e542e20d587ae14e8e
Egor <egor@ravendb.net>
2019-09-05 12:25:30 +0300

    Add try catch to test

--- test/StressTests/Cluster/ClusterStressTests.cs
+++ test/StressTests/Cluster/ClusterStressTests.cs
@@ -38,106 +38,141 @@ namespace StressTests.Cluster
         [Fact]
         public async Task ParallelClusterTransactions()
         {
-            DebuggerAttachedTimeout.DisableLongTimespan = true;
-            var numberOfNodes = 7;
-            var cluster = await CreateRaftCluster(numberOfNodes);
-            var db = GetDatabaseName();
-            using (GetDocumentStore(new Options
+            List<RavenServer> nods = null;
+            string dbName = null;
+            try
             {
-                Server = cluster.Leader,
-                ReplicationFactor = numberOfNodes,
-                ModifyDatabaseName = _ => db
-            }))
-            {
-                var count = 0;
-                var tasks = new List<Task>();
-                var random = new Random();
 
-                for (int i = 0; i < 100; i++)
+                DebuggerAttachedTimeout.DisableLongTimespan = true;
+                var numberOfNodes = 7;
+                var cluster = await CreateRaftCluster(numberOfNodes);
+                nods = cluster.Nodes;
+                var db = GetDatabaseName();
+                dbName = db;
+                using (GetDocumentStore(new Options
                 {
-                    var t = Task.Run(async () =>
-                    {
-                        var nodeNum = random.Next(0, numberOfNodes);
-                        using (var store = GetDocumentStore(new Options
-                        {
-                            Server = cluster.Nodes[nodeNum],
-                            CreateDatabase = false
-                        }))
-                        {
-                            for (int j = 0; j < 10; j++)
-                            {
-                                try
-                                {
-                                    await store.Operations.ForDatabase(db).SendAsync(new PutCompareExchangeValueOperation<User>($"usernames/{Interlocked.Increment(ref count)}", new User(), 0));
+                    Server = cluster.Leader,
+                    ReplicationFactor = numberOfNodes,
+                    ModifyDatabaseName = _ => db
+                }))
+                {
+                    var count = 0;
+                    var tasks = new List<Task>();
+                    var random = new Random();
 
-                                    using (var session = store.OpenAsyncSession(db))
+                    for (int i = 0; i < 100; i++)
+                    {
+                        var t = Task.Run(async () =>
+                        {
+                            var nodeNum = random.Next(0, numberOfNodes);
+                            using (var store = GetDocumentStore(new Options
+                            {
+                                Server = cluster.Nodes[nodeNum],
+                                CreateDatabase = false
+                            }))
+                            {
+                                for (int j = 0; j < 10; j++)
+                                {
+                                    try
                                     {
-                                        session.Advanced.SetTransactionMode(TransactionMode.ClusterWide);
-                                        session.Advanced.ClusterTransaction.CreateCompareExchangeValue($"usernames/{Interlocked.Increment(ref count)}", new User());
-                                        await session.StoreAsync(new User());
-                                        await session.SaveChangesAsync();
+                                        await store.Operations.ForDatabase(db).SendAsync(new PutCompareExchangeValueOperation<User>($"usernames/{Interlocked.Increment(ref count)}", new User(), 0));
+
+                                        using (var session = store.OpenAsyncSession(db))
+                                        {
+                                            session.Advanced.SetTransactionMode(TransactionMode.ClusterWide);
+                                            session.Advanced.ClusterTransaction.CreateCompareExchangeValue($"usernames/{Interlocked.Increment(ref count)}", new User());
+                                            await session.StoreAsync(new User());
+                                            await session.SaveChangesAsync();
+                                        }
+                                    }
+                                    catch
+                                    {
+                                        //
                                     }
                                 }
-                                catch
-                                {
-                                    //
-                                }
                             }
+                        });
+                        tasks.Add(t);
+                    }
+
+                    foreach (var task in tasks)
+                    {
+                        try
+                        {
+                            await task;
                         }
-                    });
-                    tasks.Add(t);
+                        catch
+                        {
+                            // ignore
+                        }
+                    }
+
+                    var maxTerm = cluster.Nodes.Select(x => x.ServerStore.Engine.CurrentTerm).Max();
+                    long maxTermOld;
+                    var attempts = 3 * numberOfNodes;
+                    do
+                    {
+                        maxTermOld = maxTerm;
+                        await Task.Delay(TimeSpan.FromSeconds(3) * numberOfNodes);
+                        maxTerm = cluster.Nodes.Select(x => x.ServerStore.Engine.CurrentTerm).Max();
+
+                        attempts--; // cluster couldn't stabilize
+                    } while (maxTerm != maxTermOld && attempts > 0);
+
+                    var compareExchangeCount = new HashSet<long>();
+                    var maxLog = 0L;
+
+                    foreach (var n in cluster.Nodes.Where(x => x.ServerStore.Engine.CurrentTerm >= maxTerm))
+                    {
+                        using (n.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext ctx))
+                        using (ctx.OpenReadTransaction())
+                        {
+                            var currentLog = n.ServerStore.Engine.GetLastEntryIndex(ctx);
+                            if (maxLog < currentLog)
+                                maxLog = currentLog;
+                        }
+                    }
+
+                    foreach (var node in cluster.Nodes)
+                    {
+                        await node.ServerStore.Cluster.WaitForIndexNotification(maxLog, TimeSpan.FromMinutes(1));
+
+                        using (node.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext ctx))
+                        using (ctx.OpenReadTransaction())
+                        {
+                            compareExchangeCount.Add(node.ServerStore.Cluster.GetNumberOfCompareExchange(ctx, db));
+                        }
+                    }
+
+                    Assert.Equal(1, compareExchangeCount.Count);
+                }
+            }
+            catch (Exception e)
+            {
+                if (nods != null)
+                {
+                    var compareExchangeCount = new HashSet<long>();
+                    foreach (var node in nods)
+                    {
+                        using (node.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext ctx))
+                        using (ctx.OpenReadTransaction())
+                        {
+                            var currentLog = node.ServerStore.Engine.GetLastEntryIndex(ctx);
+                            Console.WriteLine($"node: {node.ServerStore.NodeTag}, index: {currentLog}");
+                            compareExchangeCount.Add(node.ServerStore.Cluster.GetNumberOfCompareExchange(ctx, dbName));
+                        }
+                    }
+
+                    Assert.Equal(1, compareExchangeCount.Count);
+                    Console.WriteLine(e);
+                }
+                else
+                {
+                    Console.WriteLine($"nods null, dbName isNull? {dbName == null}");
                 }
 
-                foreach (var task in tasks)
-                {
-                    try
-                    {
-                        await task;
-                    }
-                    catch
-                    {
-                        // ignore
-                    }
-                }
-
-                var maxTerm = cluster.Nodes.Select(x => x.ServerStore.Engine.CurrentTerm).Max();
-                long maxTermOld;
-                var attempts = 3 * numberOfNodes;
-                do
-                {
-                    maxTermOld = maxTerm;
-                    await Task.Delay(TimeSpan.FromSeconds(3) * numberOfNodes);
-                    maxTerm = cluster.Nodes.Select(x => x.ServerStore.Engine.CurrentTerm).Max();
-
-                    attempts--; // cluster couldn't stabilize
-                } while (maxTerm != maxTermOld && attempts > 0);
-
-                var compareExchangeCount = new HashSet<long>();
-                var maxLog = 0L;
-
-                foreach (var n in cluster.Nodes.Where(x => x.ServerStore.Engine.CurrentTerm >= maxTerm))
-                {
-                    using (n.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext ctx))
-                    using (ctx.OpenReadTransaction())
-                    {
-                        var currentLog = n.ServerStore.Engine.GetLastEntryIndex(ctx);
-                        if (maxLog < currentLog)
-                            maxLog = currentLog;
-                    }
-                }
-
-                foreach (var node in cluster.Nodes)
-                {
-                    await node.ServerStore.Cluster.WaitForIndexNotification(maxLog, TimeSpan.FromMinutes(1));
-
-                    using (node.ServerStore.ContextPool.AllocateOperationContext(out TransactionOperationContext ctx))
-                    using (ctx.OpenReadTransaction())
-                    {
-                        compareExchangeCount.Add(node.ServerStore.Cluster.GetNumberOfCompareExchange(ctx, db));
-                    }
-                }
-
-                Assert.Equal(1, compareExchangeCount.Count);
+                Console.WriteLine(e);
+                throw;
             }
         }
     }
