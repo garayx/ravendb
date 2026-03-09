9f212fd5e6773e8a7ebfc2ac050a33bd4da9e64d
egor <egor@ravendb.net>
2026-03-09 17:34:24 +0200

    RavenDB-26046 - move sqlmigration modals to client

---
+++ src/Raven.Client/SqlMigration/Model/MigrationSettings.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Raven.Server.SqlMigration.Model
+{
+    
+    public sealed class MigrationSettings
+    {
+        public List<RootCollection> Collections { get; set; }
+        public int BatchSize { get; set; } = 1000;
+        public int? MaxRowsPerTable { get; set; }
+    }
+}
